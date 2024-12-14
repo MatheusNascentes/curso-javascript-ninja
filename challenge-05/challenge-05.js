dee2785f626f806d41eea1f2f9fe5f823875b0ac
Luiz <eliefe566@gmail.com>
2024-12-14 08:32:23 -0300

    challenge 05

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,20 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+// var arrayCriada = [3, 5, 7, 11, 13];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+/* function retornarArray(array){
+    return array;
+}
+*/
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+// retornarArray(arrayCriada[2]);
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +23,20 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+/* function retornandoParametros(array1, array2){
+        return array1[array2];
+}
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+// var arrayValores = [1, "ANGRA", null, true, 5.5];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+// retornandoParametros(arrayValores, 3);
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -47,30 +52,62 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// ?
+
+/*
+    function book(nomeDoLivro){
+            var objeto = {
+                meditaçõesDeMarcoAurelio : {
+                    quantidadePaginas : 230,
+                    autor : 'Marco Aurélio',
+                    editora : 'Edipro'
+                },
+                sobreABrevidadeDaVida : {
+                    quantidadePaginas : 250,
+                    autor : 'Sêneca',
+                    editora : 'Edipro'
+                },
+                manualDeEpicteto : {
+                    quantidadePaginas : 270,
+                    autor : 'Epicteto',
+                    editora : 'Edipro'
+                }
+            };
+
+        if (nomeDoLivro){
+            return objeto[nomeDoLivro];
+        } 
+
+        return objeto;
+
+    }*/
+
+ 
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+// book();
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+/*  var livro = book('meditaçõesDeMarcoAurelio');
+    var nomeDoLivro = 'meditaçõesDeMarcoAurelio' ;
+    "O livro " + ${nomeDoLivro} + " tem " + ${livro.quantidadePaginas} + " páginas!";
+*/
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
+// "O autor do livro " + ${nomeDoLivro} + " é " + ${livro.autor} + ".";
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-// ?
+// "O livro " + ${nomeDoLivro} + " foi publicado pela editora " + ${livro.editora} + ".";
