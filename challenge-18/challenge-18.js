a1a3406e70750f98223d27441dd392d2d80a918c
Luiz <eliefe566@gmail.com>
2025-02-12 20:57:55 -0300

    Challenge 18

--- challenge-18/challenge-18.js
+++ challenge-18/challenge-18.js
@@ -1,10 +1,12 @@
-/*
+(function () {
+  "use stricts";
+  /*
 1. Envolva todo o conteúdo desse desafio em uma IIFE.
 2. Adicione a diretiva 'use strict';
 3. Crie um arquivo index.html e adicione esse script à ele.
 */
 
-/*
+  /*
 Crie uma função chamada `cleanCPF`, que receba um CPF por parâmetro, e
 retorne esse CPF limpo (somente os números).
 Usando os CPFs abaixo, mostre no console que a limpeza funciona para todos
@@ -14,18 +16,29 @@ eles! Use um console.log para cada CPF.
 - "735 500 794 - 22"
 - "101.123-131x32"
 */
-console.log( 'Limpando CPFs:' );
-// ?
-
-/*
+  console.log("Limpando CPFs:");
+  function cleanCPF(cpf) {
+    return cpf.replace(/\D/g, "");
+  }
+  console.log(cleanCPF("049-214 3421-1"));
+  console.log(cleanCPF("210.458.522-05"));
+  console.log(cleanCPF("735 500 794 - 22"));
+  console.log(cleanCPF("101.123-131x32"));
+  /*
 Usando os CPFs limpos acima, deixe-os com a formatação correta de CPF.
 Ex.: "999.999.999-99"
 Mostre o resultado no console.
 */
-console.log( '\nFormatando CPFs corretamente:' );
-// ?
+  console.log("\nFormatando CPFs corretamente:");
+  function formatCPF(cpf) {
+    return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4");
+  }
+  console.log(formatCPF("04921434211"));
+  console.log(formatCPF("21045852205"));
+  console.log(formatCPF("73550079422"));
+  console.log(formatCPF("10112313132"));
 
-/*
+  /*
 Crie uma expressão regular que faça match com as palavras "junho" ou "julho",
 usando o mínimo de caracteres possíveis na regex.
 Para garantir que a regex funciona, teste-a usando o método match. Se houver
@@ -36,10 +49,12 @@ Mostre no console o resultado do match para a frase:
 O resultado deve ser:
 ["junho", "julho"]
 */
-console.log( '\nMatch com as palavras "junho" ou "julho" para a frase "Os meses de janeiro, junho e julho começam com a letra j.":' );
-// ?
+  console.log('\nMatch com as palavras "junho" ou "julho" para a frase "Os meses de janeiro, junho e julho começam com a letra j.":');
+  var phrase = "Os meses de janeiro, junho e julho começam com a letra j.";
+  var phraseRegex = phrase.match(/junho|julho/g);
+  console.log(phraseRegex);
 
-/*
+  /*
 Crie uma expressão regular que faça o match com a abertura de uma tag
 HTML qualquer.
 Ex.: "<div>", "<section>", "<blockquote>".
@@ -48,10 +63,12 @@ Use o método match e faça o teste com a marcação abaixo:
 O resultado deve ser:
 ["<div>", "<section>", "<blockquote>"]
 */
-console.log( '\nMatch com a abertura de uma tag HTML:' );
-// ?
+  console.log("\nMatch com a abertura de uma tag HTML:");
+  var tag = "<div><section><blockquote>Texto <img /></blockquote></section></div>";
+  var tagRegex = tag.match(/<\w+>/g);
+  console.log(tagRegex);
 
-/*
+  /*
 Crie uma expressão regular que faça o match com uma tag HTML vazia, casando
 com a abertura e fechamento da tag.
 Ex.: "<div></div>", "<section></section>", "<blockquote></blockquote>".
@@ -60,10 +77,11 @@ Use o método match e faça o teste com a marcação abaixo:
 O resultado deve ser:
 ["<li></li>", "<li></li>", "<span></span>"]
 */
-console.log( '\nMatch com tags HTML vazias (abertura e fechamento da tag):' );
-// ?
-
-/*
+  console.log("\nMatch com tags HTML vazias (abertura e fechamento da tag):");
+  var emptyTag = "<div><ul><li></li><li></li><li><span></span></li></ul></div>";
+  var emptyTagRegex = emptyTag.match(/(<\w+><\/\w+>)/g);
+  console.log(emptyTagRegex);
+  /*
 Vamos complicar um pouco agora :D
 
 Crie uma expressão regular que faça o match com um texto existente dentro de
@@ -85,5 +103,10 @@ Uma dica: faça o match aos poucos. Para facilitar o teste, use o site
 https://regex101.com/#javascript e verifique se as capturas estão
 corretas, para depois aplicar no código ;)
 */
-console.log( '\nFazer replace dos textos das tags:' );
-// ?
+  console.log("\nFazer replace dos textos das tags:");
+  var text = "<h1>Título da página</h1><p>Este é um parágrafo</p><footer>Rodapé</footer>";
+
+  var textRegex = text.replace(/<(\w+)>([^<]+)<\/\1>/g, `<$1>O texto dentro da tag "$1" é "$2"</$1>\n`);
+
+  console.log(textRegex);
+})();
