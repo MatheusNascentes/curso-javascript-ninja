6b28ea5afd683c71c54cc1e3aeee9a1b39f57d88
Luiz <eliefe566@gmail.com>
2025-02-27 20:04:06 -0300

    Challenge 21

--- challenge-21/challenge-21.js
+++ challenge-21/challenge-21.js
@@ -47,5 +47,6 @@ dado ao elemento HTML deve definir o que o elemento é ou o que ele faz.
 
   startButton.addEventListener("click", iniciar);
   stopButton.addEventListener("click", parar);
+  git;
   resetButton.addEventListener("click", resetar);
 })(window, document);
