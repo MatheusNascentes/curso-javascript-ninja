3fa6bab6b35f72b952e37bc1e7f822694b88fa26
Matheus Nascentes <matheus.nascentes@squadra.com.br>
2026-05-21 07:14:29 -0300

    resolucao-31

--- challenge-31/challenge-31.js
+++ challenge-31/challenge-31.js
@@ -1,14 +1,83 @@
-/*
-Agora vamos criar a funcionalidade de "remover" um carro. Adicione uma nova
-coluna na tabela, com um botão de remover.
+(function (doc) {
+    /*
+    Agora vamos criar a funcionalidade de "remover" um carro. Adicione uma nova
+    coluna na tabela, com um botão de remover.
+    
+    Ao clicar nesse botão, a linha da tabela deve ser removida.
+    
+    Faça um pull request no seu repositório, na branch `challenge-31`, e cole
+    o link do pull request no `console.log` abaixo.
+    
+    Faça um pull request, também com a branch `challenge-31`, mas no repositório
+    do curso, para colar o link do pull request do seu repo.
+    */
 
-Ao clicar nesse botão, a linha da tabela deve ser removida.
+    function app() {
+        var carros = [];
+        var $form = doc.querySelector('[data-js="formulario"]');
+        var $tabela = doc.querySelector('[data-js="tabela-carros"]');
 
-Faça um pull request no seu repositório, na branch `challenge-31`, e cole
-o link do pull request no `console.log` abaixo.
+        return {
+            init: function init() {
+                this.companyInfo();
+                this.garage();
+            },
 
-Faça um pull request, também com a branch `challenge-31`, mas no repositório
-do curso, para colar o link do pull request do seu repo.
-*/
+            companyInfo: async function companyInfo() {
+                try {
+                    const response = await fetch('./company.json');
+                    const dados = await response.json();
 
-console.log('Link do pull request do seu projeto');
+                    document.getElementById('empresa').innerText = dados.name;
+                    document.getElementById('telefone').innerText = dados.phone;
+                } catch (erro) {
+                    console.error('Erro: ', erro);
+                }
+            },
+
+            garage: function garage() {
+                $form.addEventListener('submit', this.saveCar.bind(this), false);
+            },
+
+            saveCar: function saveCar(e) {
+                e.preventDefault();
+                carros.push({
+                    imagem: $form.imagem.value,
+                    marca: $form.marca.value,
+                    ano: $form.ano.value,
+                    placa: $form.placa.value,
+                    cor: $form.cor.value
+                });
+                this.filledTable();
+            },
+
+            filledTable: function filledTable() {
+                $tabela.innerHTML = '';
+                carros.forEach((e) => {
+                    var linha = `<tr>
+                    <td>${e.imagem}</td>
+                    <td>${e.marca}</td>
+                    <td>${e.ano}</td>
+                    <td>${e.placa}</td>
+                    <td>${e.cor}</td>
+                    <td>
+                    <button data-js="deletar">Deletar</button>
+                    </td>
+                  </tr>`
+                    $tabela.innerHTML += linha;
+                });
+                this.clearData();
+            },
+            clearData: function clearData() {
+                $form.imagem.value = null;
+                $form.marca.value = null;
+                $form.ano.value = null;
+                $form.placa.value = null;
+                $form.cor.value = null;
+            }
+        };
+    }
+
+    app().init();
+
+})(document);
